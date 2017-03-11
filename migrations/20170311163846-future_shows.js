64da119241b7b3b03cae82d945c43bcbdaafa921
jamesmartin <jamesmartin442@gmail.com>
2017-03-11 16:46:37 +0000

    Added more details to runs

---
+++ migrations/20170311163846-future_shows.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   queryInterface.addColumn('runs', 'running_order', Sequelize.STRING);
+   queryInterface.addColumn('runs', 'ring_number', Sequelize.INTEGER);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.removeColumn('runs', 'running_order');
+    queryInterface.removeColumn('runs', 'ring_number');
+  }
+};

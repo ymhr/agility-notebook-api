e60bb7c29c07d783335a734f80b420d03a5e487f
jamesmartin <jamesmartin442@gmail.com>
2017-03-11 16:30:15 +0000

    Added db migrations

---
+++ migrations/20170311154044-unnamed-migration.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   queryInterface.addColumn('dogs', 'height', Sequelize.ENUM('small', 'medium', 'large'));
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+
+   queryInterface.removeColumn('dogs', 'height');
+  }
+};

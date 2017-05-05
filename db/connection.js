34d7d755d37af1dbf3a820b1c8ca4ceb8bada580
jamesmartin <jamesmartin442@gmail.com>
2017-05-05 20:52:23 +0100

    Trying to make dokku work

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
-    host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
-    pool: {
-       max: 5,
-       min: 0,
-       idle: 10000
-     }
-});
+const connectionString = process.env.DATABASE_URL || null;
+
+let db;
+
+if(connectionString) {
+    db = new Sequelize(connectionString);
+} else {
+    db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+        host: process.env.MYSQL_HOST,
+        dialect: 'mysql',
+        pool: {
+           max: 5,
+           min: 0,
+           idle: 10000
+         }
+    });
+}
 
 // const db = new Sequelize('postgres://homestead:secret@127.0.0.1:54320/agility-notebook');
 
